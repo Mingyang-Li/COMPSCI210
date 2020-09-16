98895da481da6f682b7c3fa7605d576f53924409
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2020-09-16 18:40:53 +1200

    Add files via upload

---
+++ A1/MyCode/p1.asm
@@ -0,0 +1,14 @@
+        .orig x3000
+
+        lea     r2, data
+        and     r1, r1, #0
+        add     r1, r1, #4
+
+next    add     r1, r1, #-1
+        brz     end 
+        in      
+        str     r0, r2, #0
+        add     r2, r2, #1
+        brnzp   next
+
+.END
