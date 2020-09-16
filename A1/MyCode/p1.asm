cd56f124ad87d44e6e52b112bdc10e6e022a86dc
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2020-09-16 19:49:07 +1200

    new egs

--- A1/MyCode/p1.asm
+++ A1/MyCode/p1.asm
@@ -1,4 +1,8 @@
 https://stackoverflow.com/questions/30386227/lc3-assembly-palindrome-error
+
+c1: https://www.chegg.com/homework-help/questions-and-answers/don-t-need-worry-first-two-parts-m-done-want-know-tell-user-s-input-palindrome-stop-progra-q56113580
+c2: https://www.chegg.com/homework-help/questions-and-answers/following-program-determines-whether-character-palindrome--palindrome-string-reads-backwar-q20369565
+
 .orig x3000
 
         lea     r2, data
