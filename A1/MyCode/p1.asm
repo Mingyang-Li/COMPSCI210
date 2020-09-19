820d55f12f889e12a76cbfc76bab60486c87eb94
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2020-09-19 13:50:35 +1200

    Update p1.asm

--- A1/MyCode/p1.asm
+++ A1/MyCode/p1.asm
@@ -1,35 +1,17 @@
+.ORIG X3000
 
-  .ORIG   x3100
-ENTER   LD      R0, ONE         ; Init. 0x3400 to 1
-        STI     R0, C
+lea r0,message
+puts
 
-CHECK   LDI     R0, A           ; to check if Mem[A] equals Mem[B] 
-        LDI     R1, B
-        NOT     R2, R1
-        ADD     R2, R2, #1
-        ADD     R3, R2, R0
-        BRz     YES
+ld r3, lf
+next add r4,r0,r3
+brz end
+getc
+out
+brnzp next
 
-NO      AND     R4, R4, #0      ; Set 0x3400 to 0 and exit
-        STI     R4, C
-        BRnzp   DONE
+end halt
 
-YES     LD      R0, A           ; Check if we're done
-        LD      R1, B
-        NOT     R2, R0
-        ADD     R2, R2, R1
-        BRz     DONE
-
-        ADD     R0, R0, #1      ; not done, move 'pointers'
-        ST      R0, A
-        ADD     R1, R1, #-1
-        ST      R1, B
-        BRnzp   CHECK
-
-DONE    HALT
-CNT     .FILL   x0000
-A       .FILL   x3200
-B       .FILL   x3209
-C       .FILL   x3400
-ONE     .FILL   x0001
-        .END
+message .stringz "Please enter a string: "
+lf .fill #-10
+.end
