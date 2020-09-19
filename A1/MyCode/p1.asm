f4673065cbe0bc1694a22fed3a3d368335e8ff79
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2020-09-19 14:07:50 +1200

    Update p1.asm

--- A1/MyCode/p1.asm
+++ A1/MyCode/p1.asm
@@ -3,7 +3,7 @@
 lea r0,myMessage
 puts
 
-ld r3, lf
+ld r3, enterKeyAscii
 next add r4,r0,r3
 brz end
 getc
@@ -13,5 +13,5 @@ brnzp next
 end halt
 
 myMessage .stringz "Please enter a string: "
-lf .fill #-10
+enterKeyAscii .fill #-10
 .end
