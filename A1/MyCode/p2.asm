9442e897366f5814419c47d437341e0c800ca8f7
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2020-09-21 13:06:11 +1200

    Update p2.asm

--- A1/MyCode/p2.asm
+++ A1/MyCode/p2.asm
@@ -13,13 +13,13 @@ out
 brnzp next 
 ;the above is all of part 1
 
-noc lea r0,number
-puts
-add r1,r1,#-1
-ld r0,total
-add r0,r0,r1
-out
-halt
+noc lea r0,number  ;load number into r0
+puts  ;print the output in console
+add r1,r1,#-1  ; minus 1 from r1 then store the result into r1
+ld r0,total  ;load the total from r0
+add r0,r0,r1  ;add the sum of r0 and r1 into r0
+out  ; break out of the program
+halt ;stop the program 
 
 ;every 'variable' is defiend here below
 message .stringz "Please enter a string: "
